f316ffe34dee597699f3945aca84bdffc9d7e536
BorislavKolev <sharwinchester@gmail.com>
2021-12-02 01:26:58 +0200

    Configure CSS and create first component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
+    <div id="wrapper">
+      <Home />
     </div>
   );
 }
