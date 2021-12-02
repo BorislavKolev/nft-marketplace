349c4fc935825c4e8ba791aa18c2ef9c697ed306
BorislavKolev <sharwinchester@gmail.com>
2021-12-02 23:05:07 +0200

    Install router and implement simple paths

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
+import { Routes, Route } from 'react-router-dom';
+
 import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import Register from './components/Register';
 import Login from './components/Login';
 import Explore from './components/Explore';
-import Details from './components/Details';
 import Create from './components/Create';
-
-
-
-
 
 
 function App() {
   return (
     <div id="wrapper">
       <Header />
-
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
 
       <Footer />
     </div>
