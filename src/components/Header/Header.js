69e5ef902d4ec38fb023be90cfc8d7550e2447ef
BorislavKolev <sharwinchester@gmail.com>
2021-12-02 23:15:58 +0200

    Fixed Navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,5 @@
+import { Link, } from 'react-router-dom';
+
 const Header = () => {
     return(
         <header className="transparent">
@@ -20,68 +22,14 @@ const Header = () => {
                         <div className="de-flex-col header-col-mid">
                             <ul id="mainmenu">
                                 <li>
-                                    <a href="02_dark-index.html">Home<span></span></a>
-                                    <ul>
-                                        <li><a href="03_grey-index.html">New: Grey Scheme</a></li>
-                                        <li><a href="02_dark-index.html">Homepage 1</a></li>
-                                        <li><a href="02_dark-index-2.html">Homepage 2</a></li>
-                                        <li><a href="02_dark-index-3.html">Homepage 3</a></li>
-                                        <li><a href="02_dark-index-4.html">Homepage 4</a></li>
-                                    </ul>
+                                    <Link to="/">Home<span></span></Link>                          
                                 </li>
                                 <li>
-                                    <a href="02_dark-explore.html">Explore<span></span></a>
-                                    <ul>
-                                        <li><a href="02_dark-explore.html">Explore</a></li>
-                                        <li><a href="02_dark-explore-2.html">Explore 2</a></li>
-                                        <li><a href="02_dark-collection.html">Collections</a></li>
-                                        <li><a href="02_dark-live-auction.html">Live Auction</a></li>
-                                        <li><a href="02_dark-item-details.html">Item Details</a></li>
-                                        <li><a href="02_dark-help-center.html">Help Center</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="#">Pages<span></span></a>
-                                    <ul>
-                                        <li><a href="02_dark-author.html">Author</a></li>
-                                        <li><a href="02_dark-wallet.html">Wallet</a></li>
-                                        <li><a href="02_dark-profile.html">Profile</a></li>
-                                        <li><a href="02_dark-create-options.html">Create</a></li>
-                                        <li><a href="02_dark-news.html">News</a></li>
-                                        <li><a href="02_dark-gallery.html">Gallery</a></li>
-                                        <li><a href="02_dark-login.html">Login</a></li>
-                                        <li><a href="02_dark-login-2.html">Login 2</a></li>
-                                        <li><a href="02_dark-register.html">Register</a></li>
-                                        <li><a href="02_dark-contact.html">Contact Us</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="#">Stats<span></span></a>
-                                    <ul>
-                                        <li><a href="02_dark-activity.html">Activity</a></li>
-                                        <li><a href="02_dark-rankings.html">Rankings</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="#">Elements<span></span></a>
-                                    <ul>
-                                        <li><a href="02_dark-icons-elegant.html">Elegant Icons</a></li>
-                                        <li><a href="02_dark-icons-etline.html">Etline Icons</a></li>
-                                        <li><a href="02_dark-icons-font-awesome.html">Font Awesome Icons</a></li>
-                                        <li><a href="02_dark-accordion.html">Accordion</a></li> 
-                                        <li><a href="02_dark-alerts.html">Alerts</a></li>
-                                        <li><a href="02_dark-counters.html">Counters</a></li>
-                                        <li><a href="02_dark-modal.html">Modal</a></li>
-                                        <li><a href="02_dark-popover.html">Popover</a></li>
-                                        <li><a href="02_dark-pricing-table.html">Pricing Table</a></li>
-                                        <li><a href="02_dark-progress-bar.html">Progress Bar</a></li>
-                                        <li><a href="02_dark-tabs.html">Tabs</a></li>
-                                        <li><a href="02_dark-tooltips.html">Tooltips</a></li>
-                                    </ul>
+                                    <Link to="/explore">Explore<span></span></Link>                               
                                 </li>
                             </ul>
                             <div className="menu_side_area">
-                                <a href="02_dark-wallet.html" className="btn-main btn-wallet"><i className="icon_wallet_alt"></i><span>Connect Wallet</span></a>
+                                <Link to="/create" className="btn-main btn-wallet"><i className="icon_wallet_alt"></i><span>Create NFT</span></Link>
                                 <span id="menu-btn"></span>
                             </div>
                         </div>
