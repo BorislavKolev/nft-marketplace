3de9feee6d7ff0c3bf85090e605b60800beac5ae
BorislavKolev <sharwinchester@gmail.com>
2021-12-14 02:51:22 +0200

    Created NftList component for reusability

--- client/src/components/Explore/Explore.js
+++ client/src/components/Explore/Explore.js
@@ -1,43 +1,36 @@
 import { useState, useEffect } from 'react';
-import NftCard from "./NftCard";
+import NftList from '../NftList';
 
 import * as nftService from '../../services/nftService';
 
 const Explore = () => {
-    const [nfts, setNfts] = useState([]);
+  const [nfts, setNfts] = useState([]);
 
-    useEffect(() => {
-        nftService.getAll()
-            .then(result => {
-                setNfts(result[0]);
-            })
-            .catch(err => {
-                console.log(err);
-            })
-    }, []);
+  useEffect(() => {
+    nftService.getAll()
+      .then(result => {
+        setNfts(result[0]);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, []);
 
-    return(
-        <div className="page-section border-top">
-        <p></p>
-        <div className="container">
-          <div className="text-center wow fadeInUp">
-            <div className="subhead">Buy. Sell. Trade.</div>
-            <h2 className="title-section">Explore our <span className="marked">Marketplace</span></h2>
-            <div className="divider mx-auto"></div>
-          </div>
-          <div className="row my-5 card-blog-row">
-          {nfts.length > 0
-                     ? (
-                         <>
-                         {nfts.map(x => <NftCard key={x._id} nft={x} />)}
-                         </>
-                     ) 
-                     : <p>No nfts in database!</p>
-                     }
-            
-          </div>
+  return (
+    <div className="page-section border-top">
+      <p></p>
+      <div className="container">
+        <div className="text-center wow fadeInUp">
+          <div className="subhead">Buy. Sell. Trade.</div>
+          <h2 className="title-section">Explore our <span className="marked">Marketplace</span></h2>
+          <div className="divider mx-auto"></div>
         </div>
-      </div> 
-    )}
+        <div className="row my-5 card-blog-row">
+          <NftList nfts={nfts} />
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Explore;
---
+++ client/src/components/NftList/NftList.js
@@ -0,0 +1,20 @@
+import NftCard from "./NftCard";
+
+const NftList = ({
+    nfts
+}) => {
+    return (
+        <>
+            {nfts.length > 0
+                ? (
+                    <>
+                        {nfts.map(x => <NftCard key={x._id} nft={x} />)}
+                    </>
+                )
+                : <p>Currently there are no NFTs in this section...</p>
+            }
+        </>
+    );
+}
+
+export default NftList;
